bdbfabec751f21c9de973d2b7425ff10795d78bb
ChaeMoon <jungchaemoon@gmail.com>
2019-05-03 12:07:59 +0900

    update set_base

--- summaryhelper/summaryhelp/urls.py
+++ summaryhelper/summaryhelp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from summary import views
+from summaryhelper.summary import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
