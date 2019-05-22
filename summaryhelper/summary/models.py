b6dbaead267731f3f717f14d3f7f88f115c64887
seokcheon ju <smallfish06@cau.ac.kr>
2019-05-22 15:54:26 +0900

    Revert "added model module. need to add star manage module"
    This reverts commit 691491287c811ac6fc322467d72f09cd586278e6.

--- summaryhelper/summary/models.py
+++ summaryhelper/summary/models.py
@@ -8,19 +8,11 @@ class Article(models.Model) :
 
     def __str__(self):
         return self.article_text
-    def get_articletext(self):
-        return article_text
 
 class Summary(models.Model) :
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     summary_text = models.TextField()
     star = models.IntegerField(default=0)
-    
-    textrancres=[]
+
     def __str__(self):
         return self.summary_text
-    def __init__(self,Article):
-        article=Article
-
-
-
